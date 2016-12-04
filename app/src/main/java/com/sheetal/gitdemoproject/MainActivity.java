45cb42ce9ea32bec42a2081e43d6ef50e5b1dcd5
sheetalswarnkar <sheetalswarnkar@gmail.com>
2016-12-04 13:40:57 +0530

    MainActivity.java updated successfully

--- app/src/main/java/com/sheetal/gitdemoproject/MainActivity.java
+++ app/src/main/java/com/sheetal/gitdemoproject/MainActivity.java
@@ -2,12 +2,16 @@ package com.sheetal.gitdemoproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvHelloWord ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHelloWord = (TextView)findViewById(R.id.tvHelloWord);
     }
 }
